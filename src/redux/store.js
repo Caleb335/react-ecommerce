0c1f4300c1901d29538df6beaa0867c1b4189b6f
mac <mac@macs-MacBook-Air.local>
2020-08-06 20:54:33 +0100

    corrected some codes in the redux folder

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { rootReducer } from './rootReducer';
+import rootReducer from './rootReducer';
 import logger from 'redux-logger';
 
 const middlewares = [logger]
