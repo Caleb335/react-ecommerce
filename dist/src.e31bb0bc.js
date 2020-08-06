84a2336544a4811342032343a561301683522cde
kodecheff <akunneemeka125@gmail.com>
2020-08-06 12:47:31 +0100

    dist

--- dist/src.e31bb0bc.js
+++ dist/src.e31bb0bc.js
@@ -30721,7 +30721,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36631" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38675" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
