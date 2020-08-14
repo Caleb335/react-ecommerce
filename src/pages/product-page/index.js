c03d83f4c1f3b9004b1f7103ede4f3b26f3ddcc8
Caleb335 <belac335@gmail.com>
2020-08-14 20:51:21 +0100

    responsiveness of products component

--- src/pages/product-page/index.js
+++ src/pages/product-page/index.js
@@ -13,18 +13,16 @@ const ProductPage = () => {
       </div>
 
       <div className='image-gallery'>
+        {/* instead of hard coding the UI of multiple cake cards you can use the
+         * javascript map method to represent the UI
+         * It saves you the stress of having to copy and paste on multiple lines
+         * therefore, making our code shorter and cleaner.
+         */}
         <div className='row'>
           <h2>Birthday Cakes</h2>
-
-          {/* instead of hard coding the UI of multiple cake cards you can use the
-           * javascript map method to represent the UI
-           * It saves you the stress of having to copy and paste on multiple lines
-           * therefore, making our code shorter and cleaner.
-           */}
-
           {birthdayCakes.map(birthdayCake => {
             return (
-              <div key={birthdayCake.id} birthdayCake={birthdayCake}>
+              <div key={birthdayCake.id} birthdaycake={birthdayCake} className='cakes'>
                 <div className=' column col-lg-3 col-md-4 col-sm-6 shadow' style={{ borderRadius: '50%' }}>
                   <div className='card text-center shadow-lg ' style={{ marginTop: '25px', marginBottom: '25px', borderRadius: '5%' }}>
                     <div className='px-5 pt-5'>
@@ -33,7 +31,7 @@ const ProductPage = () => {
                     <div className='card-body'>
                       <h1 className='card-title'>{birthdayCake.name}</h1>
                       <p className='price'>Amount: {birthdayCake.price}</p>
-                      <button href='#' class='btn btn-danger'>
+                      <button href='#' className='btn btn-danger'>
                         Add to cart
                       </button>
                     </div>
@@ -45,84 +43,78 @@ const ProductPage = () => {
         </div>
 
         {/* cup cakes section */}
-        <div className='cupcakes'>
-          <div className='row'>
-            <h2>Cup Cakes</h2>
-            {cupCakes.map(cupCake => {
-              return (
-                <div key={cupCake.id} cupCake={cupCake}>
-                  <div className=' column col-lg-3 col-md-4 col-sm-6' style={{ borderRadius: '50%' }}>
-                    <div className='card text-center shadow-lg ' style={{ marginTop: '25px', marginBottom: '25px', borderRadius: '5%' }}>
-                      <div className='px-5 pt-5'>
-                        <img src={cupCake.imageUrl} className='card-img-top img-fluid' alt='cup cakes' />
-                      </div>
-                      <div className='card-body'>
-                        <h1 className='card-title'>{cupCake.name}</h1>
-                        <p className='price'>Amount: {cupCake.price}</p>
-                        <button href='#' class='btn btn-danger'>
-                          Add to cart
-                        </button>
-                      </div>
+        <div className='row'>
+          <h2>Cup Cakes</h2>
+          {cupCakes.map(cupCake => {
+            return (
+              <div key={cupCake.id} cupcake={cupCake} className='cakes'>
+                <div className=' column col-lg-3 col-md-4 col-sm-6' style={{ borderRadius: '50%' }}>
+                  <div className='card text-center shadow-lg ' style={{ marginTop: '25px', marginBottom: '25px', borderRadius: '5%' }}>
+                    <div className='px-5 pt-5'>
+                      <img src={cupCake.imageUrl} className='card-img-top img-fluid' alt='cup cakes' />
+                    </div>
+                    <div className='card-body'>
+                      <h1 className='card-title'>{cupCake.name}</h1>
+                      <p className='price'>Amount: {cupCake.price}</p>
+                      <button href='#' className='btn btn-danger'>
+                        Add to cart
+                      </button>
                     </div>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
         </div>
 
         {/* wedding cakes section */}
-        <div className='cupcakes'>
-          <div className='row'>
-            <h2>Wedding Cakes</h2>
-            {weddingCakes.map(weddingCake => {
-              return (
-                <div key={weddingCake.id} weddingCake={weddingCake}>
-                  <div className=' column col-lg-3 col-md-4 col-sm-6' style={{ borderRadius: '50%' }}>
-                    <div className='card text-center shadow-lg ' style={{ marginTop: '25px', marginBottom: '25px', borderRadius: '5%' }}>
-                      <div className='px-5 pt-5'>
-                        <img src={weddingCake.imageUrl} className='card-img-top img-fluid' alt='cup cakes' />
-                      </div>
-                      <div className='card-body'>
-                        <h1 className='card-title'>{weddingCake.name}</h1>
-                        <p className='price'>Amount: {weddingCakes.price}</p>
-                        <button href='#' class='btn btn-danger'>
-                          Add to cart
-                        </button>
-                      </div>
+        <div className='row'>
+          <h2>Wedding Cakes</h2>
+          {weddingCakes.map(weddingCake => {
+            return (
+              <div key={weddingCake.id} weddingcake={weddingCake} className='cakes'>
+                <div className=' column col-lg-3 col-md-4 col-sm-6' style={{ borderRadius: '50%' }}>
+                  <div className='card text-center shadow-lg ' style={{ marginTop: '25px', marginBottom: '25px', borderRadius: '5%' }}>
+                    <div className='px-5 pt-5'>
+                      <img src={weddingCake.imageUrl} className='card-img-top img-fluid' alt='cup cakes' />
+                    </div>
+                    <div className='card-body'>
+                      <h1 className='card-title'>{weddingCake.name}</h1>
+                      <p className='price'>Amount: {weddingCakes.price}</p>
+                      <button href='#' className='btn btn-danger'>
+                        Add to cart
+                      </button>
                     </div>
                   </div>
                 </div>
-              );
-            })}
-          </div>
+              </div>
+            );
+          })}
+        </div>
 
-          {/* other types of cake */}
-          <div className='cupcakes'>
-            <div className='row'>
-              <h2>Other cakes</h2>
-              {others.map(otherTypes => {
-                return (
-                  <div key={otherTypes.id} otherTypes={otherTypes}>
-                    <div className=' column col-lg-3 col-md-4 col-sm-6' style={{ borderRadius: '50%' }}>
-                      <div className='card text-center shadow-lg ' style={{ marginTop: '25px', marginBottom: '25px', borderRadius: '5%' }}>
-                        <div className='px-5 pt-5'>
-                          <img src={otherTypes.imageUrl} className='card-img-top img-fluid' alt='cup cakes' />
-                        </div>
-                        <div className='card-body'>
-                          <h1 className='card-title'>{otherTypes.name}</h1>
-                          <p className='price'>Amount: {otherTypes.price}</p>
-                          <button href='#' class='btn btn-danger'>
-                            Add to cart
-                          </button>
-                        </div>
-                      </div>
+        {/* other types of cake */}
+        <div className='row'>
+          <h2>Other cakes</h2>
+          {others.map(otherTypes => {
+            return (
+              <div key={otherTypes.id} othertypes={otherTypes} className='cakes'>
+                <div className=' column col-lg-3 col-md-4 col-sm-6' style={{ borderRadius: '50%' }}>
+                  <div className='card text-center shadow-lg ' style={{ marginTop: '25px', marginBottom: '25px', borderRadius: '5%' }}>
+                    <div className='px-5 pt-5'>
+                      <img src={otherTypes.imageUrl} className='card-img-top img-fluid' alt='cup cakes' />
+                    </div>
+                    <div className='card-body'>
+                      <h1 className='card-title'>{otherTypes.name}</h1>
+                      <p className='price'>Amount: {otherTypes.price}</p>
+                      <button href='#' className='btn btn-danger'>
+                        Add to cart
+                      </button>
                     </div>
                   </div>
-                );
-              })}
-            </div>
-          </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
